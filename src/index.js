d02d1ad9d50883ec76568d6ab9fc006a1b0bcb20
Toms Karlis Ginters <144908700+Ginca1@users.noreply.github.com>
2023-11-02 13:24:43 +0200

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App'; // Make sure the path is correct
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// Optional: You can remove this line if you're not using it
